383441dbe48a7b5ab1d1187658592fca1bb4cb82
Vincent96034 <vhaunberger@gmail.com>
2024-11-17 18:40:17 +0100

    refactor: update FamaFrench and YFinance classes to inherit from PriceDataService and improve data handling

--- tradelib/data/famafrench.py
+++ tradelib/data/famafrench.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import zipfile
 import urllib.request
 import datetime as dt
@@ -8,84 +7,26 @@ from urllib.error import URLError
 
 import pandas as pd
 
-from tradelib.data.base import DataService
+from tradelib.data.base import PriceDataService
 from tradelib.utils.config_logger import setup_logger
 
 logger = setup_logger(__name__)
 
 
-class FamaFrench(DataService):
+class FamaFrench(PriceDataService):
     def __init__(self):
         super().__init__()
-        self.name = "Fama & French Factor Data"
-        self.homepage = "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/index.html"
-        self.data_category = "Financial"
-        self.data_types = ["monthly", "factors"]
         self._ff_df = pd.DataFrame()
         self.latest_date = None
         self._temp_dir = "tradelib/data/temp"
 
-    def _download_and_prepare_data(self) -> None:
-        """Downloads Fama & French data from the web and prepares it for
-        further processing.
-
-        Raises:
-            URLError: If an error occurs while downloading the Fama & French
-                data.
-            OSError: If an error occurs while deleting the temporary files
-                created during the process.
-        """
-        try:
-            logger.info("Downloading `Fama & French` data from the web ...")
-            ff_url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_daily_CSV.zip"
-            urllib.request.urlretrieve(
-                ff_url, f'{self._temp_dir}/fama_french.zip')
-        except URLError as url_e:
-            logger.error(
-                "An URL-Error occured when downloading `Fama & French` data: %s", url_e)
-            logger.error(
-                "Fama French data could not be downloaded. Terminating Program.")
-            sys.exit()
-        # open zip file in temp folder, create .csv, create pandas df, delete files
-        with zipfile.ZipFile(f'{self._temp_dir}/fama_french.zip', 'r') as z_file:
-            z_file.extractall(f'{self._temp_dir}/')
-            ff_factors = pd.read_csv(
-                f'{self._temp_dir}/F-F_Research_Data_Factors_daily.CSV', skiprows=4)
-        try:
-            os.remove(f'{self._temp_dir}/fama_french.zip')
-            os.remove(
-                f'{self._temp_dir}/F-F_Research_Data_Factors_daily.CSV')
-        except OSError as os_error:
-            logger.error(
-                "Could not delete temp files of `Fama & French` data: %s", os_error)
-        # process df
-        ff_factors = ff_factors.rename(columns={"Unnamed: 0": "date"})
-        ff_factors["date"] = pd.to_numeric(ff_factors["date"], errors="coerce")
-        ff_factors = ff_factors[ff_factors["date"]
-                                > 9999999]  # remove annual data
-        ff_factors = ff_factors.astype(
-            {"date": int, "Mkt-RF": float, "SMB": float, "HML": float, "RF": float})
-        ff_factors.index = ff_factors["date"]
-        ff_factors = ff_factors.drop(labels="date", axis="columns")
-        ff_factors.index = pd.to_datetime(ff_factors.index, format='%Y%m%d')
-        ff_factors = ff_factors.apply(lambda x: x / 100)
-        self._ff_df = ff_factors
-        self.latest_date = str(ff_factors.index[-1].date())
-
-    def download(
-            self,
-            start: Union[None, str] = None,
-            stop: Union[None, str] = None,
-            update: bool = False
+    def get(
+        self,
+        start: Union[None, str] = None,
+        stop: Union[None, str] = None,
+        update: bool = True,
     ) -> pd.DataFrame:
-        """Downloads the Fama & French data from the web."""
-        return self.famafrench_data_historic(start, stop, update)
-
-    def famafrench_data_historic(self,
-                                 start: Union[None, str] = None,
-                                 stop: Union[None, str] = None,
-                                 update: bool = False) -> pd.DataFrame:
-        """ Retrieves historic Fama & French data in the specified date range,
+        """Retrieves historic Fama & French data in the specified date range,
         or the entire dataset if no date range is specified.
 
         Args:
@@ -95,7 +36,7 @@ class FamaFrench(DataService):
             stop: End date of the desired date range in 'YYYY-MM-DD' format.
                 Defaults to None.
             update: If True, downloads and updates the Fama & French data
-                before returning the results. Defaults to False.
+                before returning the results. Defaults to True.
 
         Returns:
             A pandas DataFrame containing the Fama & French data in the
@@ -104,7 +45,7 @@ class FamaFrench(DataService):
 
         Example Ouput:
                     Mkt-RF	SMB	    HML	    RF
-        date				
+        date
         1926-07-31	0.0296	-0.0256	-0.0243	0.0022
         1926-08-31	0.0264	-0.0117	0.0382	0.0025
         1926-09-30	0.0036	-0.0140	0.0013	0.0023
@@ -125,5 +66,47 @@ class FamaFrench(DataService):
         try:
             dt.date.fromisoformat(date_string)
         except ValueError as exc:
-            raise ValueError(
-                "Incorrect data format, should be YYYY-MM-DD") from exc
+            raise ValueError("Incorrect data format, should be YYYY-MM-DD") from exc
+
+    def _download_and_prepare_data(self) -> None:
+        """Downloads Fama & French data from the web and prepares it for
+        further processing.
+
+        Raises:
+            URLError: If an error occurs while downloading the Fama & French
+                data.
+            OSError: If an error occurs while deleting the temporary files
+                created during the process.
+        """
+        try:
+            logger.info("Downloading `Fama & French` data from the web ...")
+            ff_url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_daily_CSV.zip"
+            urllib.request.urlretrieve(ff_url, f"{self._temp_dir}/fama_french.zip")
+        except URLError as e:
+            raise URLError(f"An error occured when downloading data: {e}")
+        # open zip file in temp folder, create .csv, create pandas df, delete files
+        with zipfile.ZipFile(f"{self._temp_dir}/fama_french.zip", "r") as z_file:
+            z_file.extractall(f"{self._temp_dir}/")
+            ff_factors = pd.read_csv(
+                f"{self._temp_dir}/F-F_Research_Data_Factors_daily.CSV", skiprows=4
+            )
+        try:
+            os.remove(f"{self._temp_dir}/fama_french.zip")
+            os.remove(f"{self._temp_dir}/F-F_Research_Data_Factors_daily.CSV")
+        except OSError as os_error:
+            logger.error(
+                "Could not delete temp files of `Fama & French` data: %s", os_error
+            )
+        # process df
+        ff_factors = ff_factors.rename(columns={"Unnamed: 0": "date"})
+        ff_factors["date"] = pd.to_numeric(ff_factors["date"], errors="coerce")
+        ff_factors = ff_factors[ff_factors["date"] > 9999999]  # remove annual data
+        ff_factors = ff_factors.astype(
+            {"date": int, "Mkt-RF": float, "SMB": float, "HML": float, "RF": float}
+        )
+        ff_factors.index = ff_factors["date"]
+        ff_factors = ff_factors.drop(labels="date", axis="columns")
+        ff_factors.index = pd.to_datetime(ff_factors.index, format="%Y%m%d")
+        ff_factors = ff_factors.apply(lambda x: x / 100)
+        self._ff_df = ff_factors
+        self.latest_date = str(ff_factors.index[-1].date())
--- tradelib/data/yfinance.py
+++ tradelib/data/yfinance.py
@@ -10,55 +10,9 @@ logger = setup_logger(__name__)
 class YFinance(FinancialDataService):
     def __init__(self):
         super().__init__()
-        self.name = "Yahoo Finance"
-        self.homepage = "https://finance.yahoo.com/"
-        self.data_category = "Financial"
-        self.data_types = ["daily", "fundamental"]
         self.min_limit = 33
-        # logger.info("The 'Yahoo! finance API' is intended for personal use "
-        #             "only. You should refer to Yahoo!'s terms of use for "
-        #             "details on your rights to use the actual data "
-        #             "downloaded rights to use the actual data downloaded "
-        #             " (https://legal.yahoo.com/us/en/yahoo/terms/product-atos/apiforydn/index.html).")
 
-    def create_tickers(self, ticker_symbols: list, session=None) -> dict:
-        """Creates a dictionary of yf.Ticker objects for the given list of
-        ticker symbols.
-
-            Args:
-                ticker_symbols (list): A list of ticker symbols as strings.
-                    session (requests.Session, optional): A requests.Session
-                    object to use for the HTTP requests. Defaults to None.
-
-            Returns:
-                dict: A dictionary of Ticker objects, where the keys are the
-                    ticker symbols and  the values are Ticker objects.
-        """
-        if not isinstance(ticker_symbols, list):
-            raise TypeError("`ticker_symbols` must be of type list.")
-        if non_valid_symbols := [
-            symbol for symbol in ticker_symbols if not isinstance(symbol, str)
-        ]:
-            raise TypeError(
-                "Not all symbols in `ticker_symbols` are of type"
-                " string. The following non valid symbols have been"
-                f" provided: {non_valid_symbols}"
-            )
-        try:
-            return yf.Tickers(ticker_symbols, session).tickers
-        except TimeoutError:
-            request = {}
-            logger.warning("Encountered TimeoutError, retrying now ...")
-            for chunk in self.split_lst(ticker_symbols, 3):
-                request = {**request, **yf.Tickers(chunk, session).tickers}
-            return request
-
-    def ticker_data_historic(
-        self, ticker_symbols: list, pct_change: bool = False, **kwargs
-    ) -> pd.DataFrame:
-        return self.download(ticker_symbols, pct_change, **kwargs)
-
-    def download(
+    def get(
         self,
         ticker_symbols: list,
         pct_change: bool = False,
@@ -128,6 +82,43 @@ class YFinance(FinancialDataService):
             df = df.iloc[1:]  # remove first row (nan)
         return df
 
+    def create_tickers(self, ticker_symbols: list, session=None) -> dict:
+        """Creates a dictionary of yf.Ticker objects for the given list of
+        ticker symbols.
+
+            Args:
+                ticker_symbols (list): A list of ticker symbols as strings.
+                    session (requests.Session, optional): A requests.Session
+                    object to use for the HTTP requests. Defaults to None.
+
+            Returns:
+                dict: A dictionary of Ticker objects, where the keys are the
+                    ticker symbols and  the values are Ticker objects.
+        """
+        if not isinstance(ticker_symbols, list):
+            raise TypeError("`ticker_symbols` must be of type list.")
+        if non_valid_symbols := [
+            symbol for symbol in ticker_symbols if not isinstance(symbol, str)
+        ]:
+            raise TypeError(
+                "Not all symbols in `ticker_symbols` are of type"
+                " string. The following non valid symbols have been"
+                f" provided: {non_valid_symbols}"
+            )
+        try:
+            return yf.Tickers(ticker_symbols, session).tickers
+        except TimeoutError:
+            request = {}
+            logger.warning("Encountered TimeoutError, retrying now ...")
+            for chunk in self.split_lst(ticker_symbols, 3):
+                request = {**request, **yf.Tickers(chunk, session).tickers}
+            return request
+
+    def ticker_data_historic(
+        self, ticker_symbols: list, pct_change: bool = False, **kwargs
+    ) -> pd.DataFrame:
+        return self.download(ticker_symbols, pct_change, **kwargs)
+
     def symbol_to_isin(self, tickers: dict) -> dict:
         """Returns a dictionary of ISIN codes for the given ticker symbols.
 
@@ -163,6 +154,3 @@ class YFinance(FinancialDataService):
         """Splits a list `lst` into `n` roughly equal parts."""
         k, m = divmod(len(lst), n)
         return (lst[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
-
-
-# TODO: error handling of yf specific errors (there are many)
